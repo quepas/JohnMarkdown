befee715410ae06835476c69acc25e10531d8324
Quepas <oneinchman.dev@gmail.com>
2014-08-31 19:35:53 +0200

    Adding first draft of main script.

---
+++ johnmarkdown.py
@@ -0,0 +1,31 @@
+__author__ = 'quepas'
+
+import os
+
+def list_markdown(directory):
+    markdown_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith(".md"):
+                markdown_files.append(root + "\\" + file)
+    return markdown_files
+
+def append_markdown(head_name, append_name, sep="\n"):
+    head_file = open(head_name, "a")
+    append_file = open(append_name, "r")
+    head_file.write(sep + append_file.read())
+
+def go(directory, sep):
+    md_files = list_markdown(".")
+    num_md_files = len(md_files)
+    if num_md_files <= 1:
+        print("Nothing to merge")
+    else:
+        print("Mergin in alphabetic order")
+        head = md_files[0]
+        for i in range(1, num_md_files):
+            append_markdown(head, md_files[i], sep)
+
+go(".", "\n\n")
+
+
